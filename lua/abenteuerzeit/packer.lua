9c30e6011ba11118a5efe4729af5ffc8f855d517
abenteuerzeit <dr.adrian.mroz@outlook.com>
2024-03-22 21:40:33 +0100

    disable $TMP setting

--- lua/abenteuerzeit/packer.lua
+++ lua/abenteuerzeit/packer.lua
@@ -35,11 +35,12 @@ return require('packer').startup(function(use)
   })
 
   use {
-			'nvim-treesitter/nvim-treesitter',
-			run = function()
-				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-				ts_update()
-			end,}
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
   use("nvim-treesitter/playground")
   use("theprimeagen/harpoon")
   use("theprimeagen/refactoring.nvim")
